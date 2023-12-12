56ead4f83ade33c405e2a655990065935b8dc67a
victor braz <victor.90braz@gmail.com>
2023-12-12 17:51:07 +0100

    add creating $department->tenant_id

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\TenantScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,22 @@ class Department extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new TenantScope);
+
+        static::creating(function($department) {
+            if(session()->has('tenant_id')) {
+                $department->tenant_id = session()->get('tenant_id');
+            }
+        });
+    }
 }
