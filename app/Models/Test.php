47adfc2b5d2d26591e09d65fa8617971006d983d
victor braz <victor.90braz@gmail.com>
2023-12-12 18:27:58 +0100

    testing

---
+++ app/Models/Test.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use App\Scopes\TenantScope;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Test extends Model
+{
+    use HasFactory;
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new TenantScope);
+
+        static::creating(function($model) {
+            if(session()->has('tenant_id')) {
+                $model->tenant_id = session()->get('tenant_id');
+            }
+        });
+    }
+}
--- tests/Feature/TenantScopeTest.php
+++ tests/Feature/TenantScopeTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\File as FacadesFile;
+use Spatie\Backtrace\File;
 use Tests\TestCase;
 
 class TenantScopeTest extends TestCase
@@ -13,6 +15,15 @@ class TenantScopeTest extends TestCase
     /** @test */
     public function test_has_a_tenant_id_on_the_migration(): void
     {
-        $this->assertTrue(condition:true);
+        $now = now();
+
+        $this->artisan('make:model Test -m');
+
+        $filename = $now->year . '_' . $now->format('m') . '_' . $now->format('d') . '_' . $now->format('h') . '_' . $now->format('i') . '_' . $now->format('s') . '_create_tests_table.php';
+
+        $this->assertTrue(FacadesFile::exists(database_path('migrations/'.$filename)));
+
+        FacadesFile::delete(database_path('migrations/'.$filename));
+        FacadesFile::delete(app_path('Test.php'));
     }
 }
