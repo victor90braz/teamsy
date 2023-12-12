efe14e4259eef8b742fca67b411109c3807bc31e
victor braz <victor.90braz@gmail.com>
2023-12-12 17:34:16 +0100

    add css to submit button

--- resources/views/livewire/department-form.blade.php
+++ resources/views/livewire/department-form.blade.php
@@ -1,3 +1,6 @@
 <div>
-    <input wire:model='name' type="text">
+    <input wire:model='name' type="text" class="border p-2 mb-2">
+    <button wire:click="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Submit
+    </button>
 </div>
