06ca588a852e3b89e9d4d265388cacbe6b3b4a06
victor braz <victor.90braz@gmail.com>
2023-12-12 14:11:15 +0100

    create a column name for Tenants

--- database/migrations/2023_12_12_130935_create_tenants_table.php
+++ database/migrations/2023_12_12_130935_create_tenants_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('tenants', function (Blueprint $table) {
             $table->id();
+            $table->string(column:'name');
             $table->timestamps();
         });
     }
