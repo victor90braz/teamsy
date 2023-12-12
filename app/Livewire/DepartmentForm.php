e032f753a9c0b2fb8babb7112153b5efd89c8a50
victor braz <victor.90braz@gmail.com>
2023-12-12 17:44:27 +0100

    created a department session

--- app/Livewire/DepartmentForm.php
+++ app/Livewire/DepartmentForm.php
@@ -8,7 +8,18 @@ use Livewire\Component;
 class DepartmentForm extends Component
 {
 
-    public $name = 'Kevin';
+    public $name = 'Accounting';
+    public $success = false;
+
+    public function submit()
+    {
+        Department::create([
+            'name' => $this->name,
+            'tenant_id' => session()->get('tenant_id')
+        ]);
+
+        $this->success = true;
+    }
 
     public function mount($departmentId = null)
     {
--- resources/views/livewire/department-form.blade.php
+++ resources/views/livewire/department-form.blade.php
@@ -3,4 +3,7 @@
     <button wire:click="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Submit
     </button>
+    @if($success)
+        <div>Saved</div>
+    @endif
 </div>
